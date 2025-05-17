5bb248b926313cbfc69ee0bd2d205594a2b9209d
darioo99 <ldario0092@gmail.com>
2025-05-17 18:06:31 -0300

    d

--- sw.js
+++ sw.js
@@ -1,11 +1,28 @@
 const CACHE_NAME = 'edu-app-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/logo192.png',
-  './icons/logo512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/logo192.png',
+  '/icons/logo512.png'
 ];
+
+// Asegurarse de que el scope del Service Worker sea correcto
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Redirigir todas las peticiones a la raíz
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
 
 self.addEventListener('install', event => {
   event.waitUntil(
