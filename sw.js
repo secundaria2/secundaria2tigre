c87ad0711a93951262f592646aafdc9961580c8b
darioo99 <ldario0092@gmail.com>
2025-05-17 17:16:27 -0300

    modificada

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  './icons/logo192.png',
+  './icons/logo512.png'
 ];
 
 self.addEventListener('install', event => {
