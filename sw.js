e420e4605b619f900190d8e0ac42438e1333db2c
darioo99 <ldario0092@gmail.com>
2025-05-17 20:56:48 -0300

    cero

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ const urlsToCache = [
   './icons/logo512.png'
 ];
 
-// Evento de instalación del Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -15,39 +14,9 @@ self.addEventListener('install', event => {
   );
 });
 
-// Evento de activación del Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Evento de fetch para cachear recursos
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => cache.put(event.request, responseToCache));
-            return response;
-          });
-      })
+      .then(response => response || fetch(event.request))
   );
 });
